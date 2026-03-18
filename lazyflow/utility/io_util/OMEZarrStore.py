0d52b064d1d6e3af6d9828402ca6dca2c84dd900
Benedikt Best <63287233+btbest@users.noreply.github.com>
2026-03-18 19:30:23 +0100

    OME-Zarr: Try reader for all file paths and web-URIs

--- lazyflow/utility/io_util/OMEZarrStore.py
+++ lazyflow/utility/io_util/OMEZarrStore.py
@@ -635,7 +635,7 @@ class OMEZarrStore(MultiscaleStore):
     @staticmethod
     def is_uri_compatible(uri: str) -> bool:
         supported_ome_zarr_schemes = ["http:", "https:", "file:", "s3:"]
-        return ZARR_EXT in uri and any(uri.startswith(scheme) for scheme in supported_ome_zarr_schemes)
+        return any(uri.startswith(scheme) for scheme in supported_ome_zarr_schemes)
 
     def get_chunk_size(self, scale_key=DEFAULT_SCALE_KEY):
         scale_key = scale_key if scale_key != DEFAULT_SCALE_KEY else self.lowest_resolution_key
